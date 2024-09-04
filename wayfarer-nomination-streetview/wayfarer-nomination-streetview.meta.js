663aa19416e4a2c4cd167259d72988a5cf2d9cd5
chewbie88 <126526471+chewbie88@users.noreply.github.com>
2024-09-04 22:27:42 +0200

    Add files via upload

---
+++ wayfarer-nomination-streetview/wayfarer-nomination-streetview.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           Wayfarer Nomination Streetview
+// @author         Chewbi88, tehstone, Tntnnbltn
+// @id             wayfarer-nomination-streetview@chewbi88
+// @version        0.4.4
+// @namespace      https://github.com/chewbie88/wayfarer-plugins/
+// @updateURL      https://github.com/chewbie88/wayfarer-plugins/raw/main/wayfarer-nomination-streetview/wayfarer-nomination-streetview.meta.js
+// @downloadURL    https://github.com/chewbie88/wayfarer-plugins/raw/main/wayfarer-nomination-streetview/wayfarer-nomination-streetview.user.js
+// @description    Add Streetview to selected nomination
+// @include        https://wayfarer.nianticlabs.com/*
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
